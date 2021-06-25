2ab77ac34ab6a7ad00befdd71a68cea44c6a7174
psiconauta <psiconauta@riseup.net>
2021-06-25 17:57:23 -0300

    Ignorar gemas de deploy en producción

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,9 @@
 set :deploy_user, ENV['DONNA_DEPLOY_USER']
 set :deploy_to, ENV['DONNA_DEPLOY_PATH']
 
+# Agregar grupos a ignorar (se:concatenan:así).
+append :bundle_without, ':deploy'
+
 server ENV['DONNA_DEPLOY_SERVER'],
   user: fetch(:deploy_user),
   roles: %w[app web db]
